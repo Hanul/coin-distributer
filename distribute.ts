6635068f1ad27c5571cc68cd24ae7049cad7f954
Hanul <hanul@hanul.me>
2022-04-01 15:52:52 +0900

    dev

--- distribute.ts
+++ distribute.ts
@@ -1,20 +1,30 @@
 import { ethers } from "ethers";
 import secure from "./secure.json";
+import target from "./target.json";
 
 const provider = new ethers.providers.JsonRpcProvider(`https://polygon-mainnet.infura.io/v3/${secure.infura}`);
 const wallet = new ethers.Wallet(secure.key, provider);
 
 (async () => {
-    //const addresses: string[] = [];
-    //for (const data of target) {
-    //    addresses.push(data.address);
-    //}
-    //console.log(addresses.length);
-
-    const address = "0x67aaB54e9F81d35B2d9Ad7Bc3b6505095618aeB0";
-    console.log(`Sending MATIC to ${address} ...`);
-    await wallet.sendTransaction({
-        to: address,
-        value: ethers.utils.parseEther("1"),
-    });
+    let on = false;
+    for (const data of target) {
+        if (data.address === "0xc05c126b633a4ef7f4be82bf39ac52078f9eb7ec") {
+            on = true;
+        } else if (on === true) {
+            console.log(`Sending MATIC to ${data.address} ...`);
+            const run = async () => {
+                try {
+                    await wallet.sendTransaction({
+                        to: data.address,
+                        value: ethers.utils.parseEther("1"),
+                    });
+                } catch (error) {
+                    console.log(error, "Retry");
+                    await run();
+                }
+            };
+            await run();
+            console.log(`Sended MATIC to ${data.address}`);
+        }
+    }
 })();
